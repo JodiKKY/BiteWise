42a5be25e3834a5cb4ef916dee55a5bbabda92b1
JodiKKY <jodiyirenkyi@gmail.com>
2024-12-03 15:08:28 +0000

    hello

--- src/Pages/Auth/LoginValidation.jsx
+++ src/Pages/Auth/LoginValidation.jsx
@@ -1,21 +1,30 @@
-function Validation(values){
-    const email_pattern = /^[^\$@]+@[^\$@]+\.[^\$@]+$/
-    const password_pattern = /^(?=,*\d)(?=,*[a-z])(?=,*[A-z])[a-zA-z0-9]{8,} $/ 
-    if(values.email === ""){
-            error.email = "Email should not be empty"
-        }
-        else if(!email_pattern.test(values.email)){
-            error.email = "Email doesn't match"
-        }else {
-                error.email = ""
-            }    
-    if(values.password === ""){
-     error.name = "Password should not be empty"}
-      else if(!password_pattern.test(values.password)){
-      error.password = "Password is incorrect"
-      }else {
-        error.password = ""
-     }       
-     return error; 
-}
-export default Validation;
+function Validation(values) {
+    let error = {};  
+  
+    
+    const email_pattern = /^[^\$@]+@[^\$@]+\.[^\$@]+$/;
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;  
+  
+    // Email Validation
+    if (values.email === "") {
+      error.email = "Email should not be empty";
+    } else if (!email_pattern.test(values.email)) {
+      error.email = "Email doesn't match the required format";
+    } else {
+      error.email = "";  
+    }
+  
+    // Password Validation
+    if (values.password === "") {
+      error.password = "Password should not be empty";
+    } else if (!password_pattern.test(values.password)) {
+      error.password = "Password is incorrect. It must be at least 8 characters long, include 1 uppercase letter, 1 lowercase letter, and 1 number";
+    } else {
+      error.password = "";  
+    }
+  
+    return error;  
+  }
+  
+  export default Validation;
+  
--- src/Pages/Auth/SignupValidation.jsx
+++ src/Pages/Auth/SignupValidation.jsx
@@ -1,28 +1,44 @@
-function Validation(values){
-    let error = {}
-    const email_pattern = /^[^\$@]+@[^\$@]+\.[^\$@]+$/
-    const password_pattern = /^(?=,*\d)(?=,*[a-z])(?=,*[A-z])[a-zA-z0-9]{8,} $/ 
-    if(values.firstName === ""){
-    error.name = "First Name should not be empty"}
-    else {
-        error.firstName = ""
+function Validation(values) {
+    let error = {};
+  
+    // Email and password regex patterns
+    const email_pattern = /^[^\$@]+@[^\$@]+\.[^\$@]+$/;
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;  // Password must have at least 1 number, 1 lowercase, 1 uppercase, and at least 8 characters.
+  
+    // First Name Validation
+    if (values.firstName === "") {
+      error.firstName = "First Name should not be empty";
+    } else {
+      error.firstName = "";
     }
-    if(values.lastName === ""){
-        error.name = "Last Name should not be empty"}
-        else {
-            error.lastName = ""
-        }
-    if(values.email === ""){
-        error.email = "Email should not be empty"}
-            else {
-                error.email = ""
-            }    
-    if(values.password === ""){
-                error.name = "Password should not be empty"}
-                else {
-                    error.password = ""
-                }        
-                return error; 
-            }
-            export default Validation;
-
+  
+    // Last Name Validation
+    if (values.lastName === "") {
+      error.lastName = "Last Name should not be empty";
+    } else {
+      error.lastName = "";
+    }
+  
+    // Email Validation
+    if (values.email === "") {
+      error.email = "Email should not be empty";
+    } else if (!email_pattern.test(values.email)) {
+      error.email = "Invalid email format";
+    } else {
+      error.email = "";
+    }
+  
+    // Password Validation
+    if (values.password === "") {
+      error.password = "Password should not be empty";
+    } else if (!password_pattern.test(values.password)) {
+      error.password = "Password must be at least 8 characters long, include 1 uppercase letter, 1 lowercase letter, and 1 number";
+    } else {
+      error.password = "";
+    }
+  
+    return error;
+  }
+  
+  export default Validation;
+  
